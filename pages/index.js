e2542b91406e8ea7575cdef9097241ed85bfbc25
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-02 14:49:14 -0700

    adds wallet button

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Web3Modal from "web3modal";
 import { providers, Contract } from "ethers";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import { CONTRACT_ADDRESS, CONTRACT_ABI } from "../constants";
 
 export default function Home() {
@@ -11,30 +11,30 @@ export default function Home() {
   const web3ModalRef = useRef();
 
   const getProviderOrSigner = async (needSigner = false) => {
-    const provider = await web3ModalRef.current.connect();
-    const web3Provider = new providers.Web3Provider(provider);
+    const instance = await web3ModalRef.current.connect();
+    const provider = new providers.Web3Provider(instance);
 
-    const { chainId } = await web3Provider.getNetwork();
+    const { chainId } = await provider.getNetwork();
     if (chainId !== 4) {
       window.alert("Change the network to Rinkeby");
       throw new Error("Change network to Rinkeby");
     }
 
     if (needSigner) {
-      const signer = web3Provider.getSigner();
+      const signer = provider.getSigner();
       return signer;
     }
-    return web3Provider;
+    return provider;
   };
 
-  const connectWallet = async () => {
+  const connectWallet = useCallback(async () => {
     try {
       await getProviderOrSigner();
       setWalletConnected(true);
     } catch (err) {
       console.error(err);
     }
-  };
+  }, []);
 
   useEffect(() => {
     if (!walletConnected) {
@@ -43,8 +43,9 @@ export default function Home() {
         providerOptions: {},
         disableInjectedProvider: false,
       });
+      connectWallet();
     }
-  }, [walletConnected]);
+  }, [walletConnected, connectWallet]);
 
   return (
     <div>
@@ -54,6 +55,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>To-Do List Dapp</h1>
+      <div>
+        {!walletConnected && (
+          <button onClick={connectWallet}>Connect Wallet</button>
+        )}
+      </div>
     </div>
   );
 }
