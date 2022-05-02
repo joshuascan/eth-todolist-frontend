6c29c589149ca4e260029a2601e746a8e1d45956
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2022-05-02 14:08:29 -0700

    adds getProviderOrSigner & connectWallet functions

--- pages/index.js
+++ pages/index.js
@@ -6,6 +6,36 @@ import { useEffect, useRef, useState } from "react";
 import { CONTRACT_ADDRESS, CONTRACT_ABI } from "../constants";
 
 export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    const provider = await web3ModalRef.current.connect();
+    const web3Provider = new providers.Web3Provider(provider);
+
+    const { chainId } = await web3Provider.getNetwork();
+    if (chainId !== 4) {
+      window.alert("Change the network to Rinkeby");
+      throw new Error("Change network to Rinkeby");
+    }
+
+    if (needSigner) {
+      const signer = web3Provider.getSigner();
+      return signer;
+    }
+    return web3Provider;
+  };
+
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <div>
       <Head>
